21a98524d34278864b7d02ab5ce63252e6b98b69
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-30 04:07:01 +0300

    add asm examples

---
+++ 02_VariousExamples/00_UnnamedExamples/01/addition.asm
@@ -0,0 +1,8 @@
+ASSUME CS: CODE , DS DATA
+CODE SEGMENT
+    START :
+    MOV AX,2H
+    MOV BX,3H
+    ADD AX,BX ;addition of two registers and it stores the result in AX
+    ENDS
+ENDS START
---
+++ 02_VariousExamples/00_UnnamedExamples/01/division.asm
@@ -0,0 +1,13 @@
+ASSUME CS : CODE , DS : DATA
+
+CODE SEGMENT:
+
+    START:
+
+    MOV AX, 005H
+    MOV BX, 005H
+    DIV BX
+
+    ENDS
+
+ENDS START
