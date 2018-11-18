a25882f8465a0e5adbd971d74bf32993a56fa1c0
Zeynep <zeynep.danishh@gmail.com>
2018-11-18 15:51:27 +0300

    :tada::fire::whale::boom::apple

---
+++ 06_MathematicalCodes/Sort_Array_of_Numbers.asm
@@ -0,0 +1,38 @@
+;Array Sorting smallest to biggest
+ortaksg  SEGMENT PARA 'kod'
+         ORG 100H
+		 ASSUME ss:ortaksg,ds:ortaksg,cs:ortaksg
+Basla:   JMP ANA
+		 dizi db -7,-14,1
+		 n dw 3
+ANA      PROC NEAR
+         
+         XOR si,si
+         XOR di,di
+         MOV dx,0
+ whilee: MOV cx,n
+         DEC cx 
+         CMP dx,cx 
+         JGE bitir      
+	     PUSH si
+	     INC si
+	     MOV di,si
+		 POP si
+		 MOV al,dizi[si]
+		 SUB cx,si 
+	 L1: CMP al,dizi[di]
+		 JGE degis
+		 JMP son
+  degis: XCHG al,dizi[di]
+         MOV dizi[si],al
+    son: INC di
+         Loop L1
+	     INC si 
+	     ADD dx,1
+	     JMP whilee  
+	     
+		
+  bitir: RET
+ANA      ENDP
+ortaksg  ENDS
+         END Basla
---
+++ 06_MathematicalCodes/Sum(32+32)bit.asm
@@ -0,0 +1,28 @@
+;dosboxta -u komutu kodunuzu gosterir
+;kodunuzu ilerletmis olmadiginizdan hemen sonucu goremezsiniz
+;sonucu gormek icin trace atmaya devam edin
+ortaksg    SEGMENT PARA 'kod'
+           ORG 100h
+		   ASSUME ss:ortaksg,ds:ortaksg,cs:ortaksg
+Basla:	   JMP ANA
+		   sayi1 dd 0FFFFFFFFh
+		   sayi2 dd 0FFFFFFFFh
+		   toplam dq 0
+ANA        PROC NEAR
+           LEA si,sayi1           ;2+EA
+		   LEA di,sayi2           ;2+EA
+		   LEA bx,toplam          ;2+EA
+		   MOV ax,[si]            ;8+EA
+		   MOV dx,[di]            ;8+EA
+		   ADD ax,dx              ;3
+		   MOV [bx],ax            ;9+EA
+		   MOV ax,[si+2]          ;8+EA
+		   MOV dx,[di+2]          ;8+EA
+		   ADC ax,dx              ;3
+	       MOV [bx+2],ax          ;9+EA
+		   ADC WORD PTR [bx+4],0  ;17+EA
+		                          ;79 clock cycle
+           RET
+ANA        ENDP
+ortaksg    ENDS
+           END Basla
