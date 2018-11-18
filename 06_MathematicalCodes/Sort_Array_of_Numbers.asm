86667b108ce465dc94effa33fa45d10169a91e84
Zeynep <zeynep.danishh@gmail.com>
2018-11-18 15:57:25 +0300

    :tada::tada::apple::apple::apple::tada::tada:

--- 06_MathematicalCodes/Sort_Array_of_Numbers.asm
+++ 06_MathematicalCodes/Sort_Array_of_Numbers.asm
@@ -1,12 +1,11 @@
 ;Array Sorting smallest to biggest
 ortaksg  SEGMENT PARA 'kod'
          ORG 100H
-		 ASSUME ss:ortaksg,ds:ortaksg,cs:ortaksg
+	 ASSUME ss:ortaksg,ds:ortaksg,cs:ortaksg
 Basla:   JMP ANA
-		 dizi db -7,-14,1
-		 n dw 3
+	    dizi db -7,-14,1
+            n dw 3
 ANA      PROC NEAR
-         
          XOR si,si
          XOR di,di
          MOV dx,0
@@ -14,25 +13,24 @@ ANA      PROC NEAR
          DEC cx 
          CMP dx,cx 
          JGE bitir      
-	     PUSH si
-	     INC si
-	     MOV di,si
-		 POP si
-		 MOV al,dizi[si]
-		 SUB cx,si 
-	 L1: CMP al,dizi[di]
-		 JGE degis
-		 JMP son
+	 PUSH si
+	 INC si
+	 MOV di,si
+	 POP si
+	 MOV al,dizi[si]
+	 SUB cx,si 
+     L1: CMP al,dizi[di]
+	 JGE degis
+	 JMP son
   degis: XCHG al,dizi[di]
          MOV dizi[si],al
     son: INC di
          Loop L1
-	     INC si 
-	     ADD dx,1
-	     JMP whilee  
-	     
-		
-  bitir: RET
+	 INC si 
+	 ADD dx,1
+	 JMP whilee  	
+bitir:   RET
+
 ANA      ENDP
 ortaksg  ENDS
          END Basla
