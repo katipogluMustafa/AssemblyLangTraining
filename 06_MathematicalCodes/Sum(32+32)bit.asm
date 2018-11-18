f4ab19cfe1d0ab560ce7ddc5823cc99f8d663dd4
Zeynep <zeynep.danishh@gmail.com>
2018-11-18 15:59:12 +0300

    :tada::apple::tada::apple::tada::apple::tada:

--- 06_MathematicalCodes/Sum(32+32)bit.asm
+++ 06_MathematicalCodes/Sum(32+32)bit.asm
@@ -8,20 +8,20 @@ Basla:	   JMP ANA
 		   sayi1 dd 0FFFFFFFFh
 		   sayi2 dd 0FFFFFFFFh
 		   toplam dq 0
-ANA        PROC NEAR
-           LEA si,sayi1           ;2+EA
-		   LEA di,sayi2           ;2+EA
-		   LEA bx,toplam          ;2+EA
-		   MOV ax,[si]            ;8+EA
-		   MOV dx,[di]            ;8+EA
-		   ADD ax,dx              ;3
-		   MOV [bx],ax            ;9+EA
-		   MOV ax,[si+2]          ;8+EA
-		   MOV dx,[di+2]          ;8+EA
-		   ADC ax,dx              ;3
-	       MOV [bx+2],ax          ;9+EA
-		   ADC WORD PTR [bx+4],0  ;17+EA
-		                          ;79 clock cycle
+ANA         PROC NEAR
+            LEA si,sayi1           ;2+EA
+	    LEA di,sayi2           ;2+EA
+	    LEA bx,toplam          ;2+EA
+            MOV ax,[si]            ;8+EA
+            MOV dx,[di]            ;8+EA
+            ADD ax,dx              ;3
+            MOV [bx],ax            ;9+EA
+	    MOV ax,[si+2]          ;8+EA
+	    MOV dx,[di+2]          ;8+EA
+	    ADC ax,dx              ;3
+	    MOV [bx+2],ax          ;9+EA
+	    ADC WORD PTR [bx+4],0  ;17+EA
+	                           ;79 clock cycle
            RET
 ANA        ENDP
 ortaksg    ENDS
